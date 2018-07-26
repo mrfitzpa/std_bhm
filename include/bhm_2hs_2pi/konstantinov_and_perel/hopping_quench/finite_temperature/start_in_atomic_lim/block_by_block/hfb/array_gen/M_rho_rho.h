59f7f9dde742109579bd09f48dcb44a89a9d5e48
bonedog3000 <mrfitzpa@sfu.ca>
2018-07-26 12:45:12 -0700

    Added the array_gen::M_rho_rho function.

---
+++ include/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/array_gen/M_rho_rho.h
@@ -0,0 +1,91 @@
+// std_bhm/include/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench
+// /finite_temperature/start_in_atomic_lim/block_by_block/hfb/array_gen
+// /M_rho_rho.h
+
+// -----------------------------------------------------------------------
+
+// Insert summary of file
+
+// -----------------------------------------------------------------------
+
+#ifndef STD_BHM_BHM_2HS_2PI_KONSTANTINOV_AND_PEREL_HOPPING_QUENCH_\
+FINITE_TEMPERATURE_START_IN_ATOMIC_LIM_BLOCK_BY_BLOCK_HFB_ARRAY_GEN_\
+M_RHO_RHO_H
+#define STD_BHM_BHM_2HS_2PI_KONSTANTINOV_AND_PEREL_HOPPING_QUENCH_\
+FINITE_TEMPERATURE_START_IN_ATOMIC_LIM_BLOCK_BY_BLOCK_HFB_ARRAY_GEN_\
+M_RHO_RHO_H
+
+/* Include standard libraries */
+#include <complex>
+#include <vector>
+
+/* Non-standard third-party libraries */
+
+/* Include user-defined header files */
+
+
+
+namespace std_bhm
+{
+namespace bhm_2hs_2pi
+{
+namespace konstantinov_and_perel
+{
+namespace hopping_quench
+{
+namespace finite_temperature
+{
+namespace start_in_atomic_lim
+{
+namespace block_by_block
+{
+namespace hfb
+{
+namespace array_gen
+{
+
+
+
+namespace atomic_func_params_detail
+{
+class atomic_func_params; // forward declaration
+}
+
+
+
+namespace M_rho_rho_detail
+{
+
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::array_gen::atomic_func_params_detail;
+
+using cmplx_dbl = std::complex<double>;
+using cmplx_vec = std::vector<cmplx_dbl>;
+
+cmplx_vec M_rho_rho(const NSA3::atomic_func_params& af_params);
+
+} // end of M_rho_rho_detail namespace
+
+
+
+// lift relevant classes and functions up one namespace
+using std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench::\
+finite_temperature::start_in_atomic_lim::block_by_block::hfb::array_gen::\
+M_rho_rho_detail::M_rho_rho;
+
+
+
+} // end of array_gen namespace
+} // end of hfb namespace
+} // end of block_by_block namespace
+} // end of start_in_atomic_lim namespace
+} // end of finite_temperature namespace
+} // end of hopping_quench namespace
+} // end of konstantinov_and_perel namespace
+} // end of bhm_2hs_2pi namespace
+} // end of std_bhm namespace
+
+
+#endif
+ 
