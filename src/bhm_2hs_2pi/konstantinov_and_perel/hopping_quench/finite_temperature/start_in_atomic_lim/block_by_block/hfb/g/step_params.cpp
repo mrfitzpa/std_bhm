e9303c6287eae43199d62226759e84e9e4dfb093
bonedog3000 <mrfitzpa@sfu.ca>
2018-07-27 09:24:09 -0700

    Added the g::step_params class.

---
+++ src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/g/step_params.cpp
@@ -0,0 +1,74 @@
+// /std_bhm/src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench
+// /finite_temperature/start_in_atomic_lim/block_by_block/hfb/g/step_params.cpp
+
+// -----------------------------------------------------------------------
+
+// Insert summary of file
+
+// -----------------------------------------------------------------------
+
+
+/* Include standard libraries */
+#include <memory>
+
+/* Non-standard third-party libraries */
+#include "third_party/spimpl.h"
+
+/* Include user-defined header files */
+#include "bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/\
+start_in_atomic_lim/block_by_block/hfb/g/step_params.h"
+
+
+
+// Partial definition of impl class.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+struct NSA3::step_params::impl
+{
+    impl(int Ns, double ds);
+
+    const int Ns;
+    const double ds;
+};
+
+
+
+// step_params constructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+NSA3::step_params::step_params(int Ns, double ds)
+    : pimpl{ spimpl::make_impl<impl>(Ns, ds) }
+{}
+
+
+
+// step_params::impl constructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+NSA3::step_params::impl::impl(int Ns, double ds)
+    : Ns{Ns}, ds{ds}
+{}
+
+
+
+// Get # of time steps Ns.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+int NSA3::step_params::get_Ns() const { return pimpl->Ns; }
+
+
+
+// Get time stepsize ds.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+double NSA3::step_params::get_ds() const { return pimpl->ds; }
