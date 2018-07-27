8247fb49cb40ff0078319a30922ba462bbcd4b67
bonedog3000 <mrfitzpa@sfu.ca>
2018-07-27 16:15:54 -0700

    Added the Y::base class files.

---
+++ include/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/Y/base.h
@@ -0,0 +1,66 @@
+// std_bhm/include/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench
+// /finite_temperature/start_in_atomic_lim/block_by_block/hfb/Y/base.h
+
+// -----------------------------------------------------------------------
+
+// Insert summary of file
+
+// -----------------------------------------------------------------------
+
+#ifndef STD_BHM_BHM_2HS_2PI_KONSTANTINOV_AND_PEREL_HOPPING_QUENCH_\
+FINITE_TEMPERATURE_START_IN_ATOMIC_LIM_BLOCK_BY_BLOCK_HFB_Y_BASE_H
+#define STD_BHM_BHM_2HS_2PI_KONSTANTINOV_AND_PEREL_HOPPING_QUENCH_\
+FINITE_TEMPERATURE_START_IN_ATOMIC_LIM_BLOCK_BY_BLOCK_HFB_Y_BASE_H
+
+/* Include standard libraries */
+
+/* Non-standard third-party libraries */
+
+/* Include user-defined header files */
+
+
+
+namespace std_bhm
+{
+namespace bhm_2hs_2pi
+{
+namespace konstantinov_and_perel
+{
+namespace hopping_quench
+{
+namespace finite_temperature
+{
+namespace start_in_atomic_lim
+{
+namespace block_by_block
+{
+namespace hfb
+{
+namespace Y
+{
+
+class base
+{
+public:
+    base();
+    virtual ~base();
+
+    double eval(int l1, int l2) const;
+
+private:
+    virtual double do_eval(int l1, int l2) const = 0;
+};
+
+} // end of Y namespace
+} // end of hfb namespace
+} // end of block_by_block namespace
+} // end of start_in_atomic_lim namespace
+} // end of finite_temperature namespace
+} // end of hopping_quench namespace
+} // end of konstantinov_and_perel namespace
+} // end of bhm_2hs_2pi namespace
+} // end of std_bhm namespace
+
+
+#endif
+ 
---
+++ src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/Y/base.cpp
@@ -0,0 +1,47 @@
+// /std_bhm/src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench
+// /finite_temperature/start_in_atomic_lim/block_by_block/hfb/Y/base.cpp
+
+// -----------------------------------------------------------------------
+
+// Insert summary of file
+
+// -----------------------------------------------------------------------
+
+
+/* Include standard libraries */
+
+/* Non-standard third-party libraries */
+
+/* Include user-defined header files */
+#include "bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/\
+start_in_atomic_lim/block_by_block/hfb/Y/base.h"
+
+
+
+// base constructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::Y;
+
+NSA3::base::base() {}
+
+
+
+// Evaluate Y.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::Y;
+
+double NSA3::base::eval(int l1, int l2) const
+{
+    return do_eval(l1, l2);
+}
+
+
+
+// base destructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::Y;
+
+NSA3::base::~base() {}
