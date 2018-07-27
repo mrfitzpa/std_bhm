b388e1a58b485bdc508f447d3be32a432c2cc627
bonedog3000 <mrfitzpa@sfu.ca>
2018-07-27 11:11:54 -0700

    Added the g::base class.

---
+++ src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/g/base.cpp
@@ -0,0 +1,44 @@
+// /std_bhm/src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench
+// /finite_temperature/start_in_atomic_lim/block_by_block/hfb/g/base.cpp
+
+// -----------------------------------------------------------------------
+
+// Insert summary of file
+
+// -----------------------------------------------------------------------
+
+
+/* Include standard libraries */
+
+/* Non-standard third-party libraries */
+
+/* Include user-defined header files */
+#include "bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/\
+start_in_atomic_lim/block_by_block/hfb/g/base.h"
+
+
+
+// base constructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+NSA3::base::base() {}
+
+
+
+// Evaluate base
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+double NSA3::base::eval(int l1, int l2) const { return do_eval(l1, l2); }
+
+
+
+// base destructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb::g;
+
+NSA3::base::~base() {}
