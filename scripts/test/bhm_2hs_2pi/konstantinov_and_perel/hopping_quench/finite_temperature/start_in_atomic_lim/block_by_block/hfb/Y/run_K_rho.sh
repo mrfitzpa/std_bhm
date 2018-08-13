e12fedfeac1028607385292dba7c22804835e7ff
bonedog3000 <mrfitzpa@sfu.ca>
2018-08-13 14:26:48 -0700

    Added the Y::K_rho class files.

---
+++ scripts/test/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/Y/run_K_rho.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------------
+#
+# This script executes the program 'test/bhm_2hs_2pi/konstantinov_and_perel
+# /hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb
+# /X/K_rho'
+#
+#---------------------------------------------------------------------------
+
+# Set fixed parameters
+beta="10.0"
+mu="0.42"
+max_band_index="100"
+
+jf="0.03"
+tc="5.0"
+tau_Q="0.1"
+
+L="50"
+k_ints="5 2 1"
+
+ds="0.05"
+Ns="4001"
+step_offset="-0.5"
+
+l1="804"
+l2="663"
+
+# Get absolute path of executable/program. This is done by first getting
+# the absolute path of this script and then modifying it accordingly.
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+exe_dir=${script_dir/\/scripts\//\/bin\/}
+exe_path=$exe_dir\/K_rho
+
+time "$exe_path" <<EOF
+${beta}
+${mu}
+${max_band_index}
+${jf}
+${tc}
+${tau_Q}
+${L}
+${k_ints}
+${Ns}
+${ds}
+${step_offset}
+${l1}
+${l2}
+EOF
---
+++ test/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/Y/K_rho.cpp
@@ -0,0 +1,58 @@
+// /std_bhm/test/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench
+// /finite_temperature/start_in_atomic_lim/block_by_block/hfb/Y/K_rho.cpp
+
+// -----------------------------------------------------------------------
+
+// This program serves as a unit test of the class(es) defined in the 
+// .cpp file 'std_bhm/test/bhm_2hs_2pi/konstantinov_and_perel
+// /hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block
+// /hfb/Y/K_rho.cpp'.
+
+// -----------------------------------------------------------------------
+
+
+/* Include standard libraries */
+#include <limits>
+#include <iostream>
+
+/* Non-standard third-party libraries */
+
+/* Include user-defined header files */
+#include "bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/\
+start_in_atomic_lim/block_by_block/hfb/Y/K_rho.h"
+#include "bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/\
+start_in_atomic_lim/block_by_block/hfb/Y/params.h"
+
+#include "bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/\
+start_in_atomic_lim/block_by_block/hfb/from_std_cin/constr_Y_params.h"
+
+#include "parameters/from_std_cin.h"
+
+
+
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb;
+namespace NSA4 = NSA3::from_std_cin;
+namespace NSA5 = NSA3::Y;
+
+namespace NSA6 = std_bhm::parameters::from_std_cin;
+
+using local_params = std_bhm::atomic_lim::local::params;
+
+int main(int argc, char** argv)
+{
+    const auto y_params = ::NSA4::constr_Y_params();
+    const auto& Y_K_rho = ::NSA5::K_rho(y_params);
+
+    const auto l1 = ::NSA6::to_value<int>();
+    const auto l2 = ::NSA6::to_value<int>();
+
+    typedef std::numeric_limits<double> dbl_lim;
+    std::cout.precision(dbl_lim::max_digits10);
+
+    std::cout << "Y_K_rho(" << l1 << ", " << l2 << ") = "
+	      << Y_K_rho.eval(l1, l2) << std::endl;
+    
+    return 0;
+}
