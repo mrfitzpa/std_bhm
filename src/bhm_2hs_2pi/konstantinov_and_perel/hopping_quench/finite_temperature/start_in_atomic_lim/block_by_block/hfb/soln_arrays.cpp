e21d15a50125273400173b4c88b230b300a37dd7
bonedog3000 <mrfitzpa@sfu.ca>
2018-08-23 08:58:21 -0700

    Added the hfb::soln_arrays class files.

---
+++ src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/start_in_atomic_lim/block_by_block/hfb/soln_arrays.cpp
@@ -0,0 +1,91 @@
+// /std_bhm/src/bhm_2hs_2pi/konstantinov_and_perel/hopping_quench
+// /finite_temperature/start_in_atomic_lim/block_by_block/hfb/soln_arrays.cpp
+
+// -----------------------------------------------------------------------
+
+// Insert summary of file
+
+// -----------------------------------------------------------------------
+
+
+/* Include standard libraries */
+#include <complex>
+#include <vector>
+#include <memory>
+
+/* Non-standard third-party libraries */
+#include "third_party/spimpl.h"
+
+/* Include user-defined header files */
+#include "bhm_2hs_2pi/konstantinov_and_perel/hopping_quench/finite_temperature/\
+start_in_atomic_lim/block_by_block/hfb/soln_arrays.h"
+
+
+
+// Partial definition of impl class.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb;
+
+using cmplx_dbl = std::complex<double>;
+using cmplx_vec = std::vector<cmplx_dbl>;
+
+struct NSA3::soln_arrays::impl
+{
+    impl(cmplx_vec& y_rho, cmplx_vec& y_K);
+
+    cmplx_vec& y_rho;
+    cmplx_vec& y_K;
+};
+
+
+
+// soln_arrays constructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb;
+
+NSA3::soln_arrays::soln_arrays(cmplx_vec& y_rho, cmplx_vec& y_K)
+    : pimpl{ spimpl::make_impl<impl>(y_rho, y_K) }
+{}
+
+
+
+// soln_arrays::impl constructor.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb;
+
+NSA3::soln_arrays::impl::impl(cmplx_vec& y_rho, cmplx_vec& y_K)
+    : y_rho(y_rho), y_K(y_K)
+{}
+
+
+
+// Get y_rho array representation.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb;
+
+using cmplx_dbl = std::complex<double>;
+using cmplx_vec = std::vector<cmplx_dbl>;
+
+cmplx_vec& NSA3::soln_arrays::get_y_rho() const
+{
+    return pimpl->y_rho;
+}
+
+
+
+// Get y_K array representation.
+namespace NSA1 = std_bhm::bhm_2hs_2pi::konstantinov_and_perel::hopping_quench;
+namespace NSA2 = NSA1::finite_temperature::start_in_atomic_lim::block_by_block;
+namespace NSA3 = NSA2::hfb;
+
+using cmplx_dbl = std::complex<double>;
+using cmplx_vec = std::vector<cmplx_dbl>;
+
+cmplx_vec& NSA3::soln_arrays::get_y_K() const
+{
+    return pimpl->y_K;
+}
